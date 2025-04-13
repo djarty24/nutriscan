8e4088a2e3592f7e31ee655214fce95e084e7602
djarty24 <24revati@gmail.com>
2025-04-13 15:18:12 -0700

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './components/Home';
 import ScanBarcode from './components/ScanBarcode';
 import ProductInfo from './components/ProductInfo';
 import Navbar from './components/Navbar';
 
 const App = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/scan" element={<ScanBarcode />} />
-                <Route path="/product-info/:barcode" element={<ProductInfo />} />
-            </Routes>
-        </Router>
-    );
+	return (
+		<Router>
+			<ScrollToTop />
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/scan" element={<ScanBarcode />} />
+				<Route path="/product-info/:barcode" element={<ProductInfo />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
